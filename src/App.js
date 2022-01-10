3dee7732c15ffb2931465a916394aa22c3d019c0
rosgas <rgasbarri.274@gmail.com>
2022-01-10 10:58:47 +1000

    Initial & Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Background from "./components/assets/main-bg.png";
 
 function App() {
+  const divStyle = {
+    background: "url(" + Background + ") center center/cover",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div
+        className="flex flex-col items-center justify-between h-screen"
+        style={divStyle}
+      >
+        <Navbar />
+
+        <main>Main Content</main>
+        <footer>Footer</footer>
+      </div>
+    </Router>
   );
 }
 
