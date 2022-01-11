8777a069fe28e76fddbe03fb948c02f5020203ce
rosgas <rgasbarri.274@gmail.com>
2022-01-11 17:03:29 +1000

    Layout

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,32 @@
+function Footer() {
+  return (
+    <footer className="w-screen py-6 bg-sky-900/50">
+      <div className="container mx-auto text-center text-slate-400 text-sm">
+        <p>
+          You can find the GitHub repository of this coding project{" "}
+          <a
+            href="https://github.com/rosgas/react-github-finder"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            here
+          </a>
+        </p>
+        <p>
+          Background vector created by freepik -{" "}
+          <a
+            href="https://www.freepik.com/vectors/background"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            www.freepik.com
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
